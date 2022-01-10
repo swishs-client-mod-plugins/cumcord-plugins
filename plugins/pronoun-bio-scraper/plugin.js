abb2e644201e1b4b9c5e6fac44a5a1b8d2753ad5
Swishilicous <Swishilicous@users.noreply.github.com>
2022-01-10 22:35:37 +0000

    deploy: d4bdfd31652a997625b78faf1cbfe6b3cbacc7be

--- plugins/pronoun-bio-scraper/plugin.js
+++ plugins/pronoun-bio-scraper/plugin.js
@@ -1,4 +1,4 @@
-(function(r,M,a,t,f,R){"use strict";/**
+(function(r,s,y,t,R,f){"use strict";/**
 * NOTICE OF LICENSE
 *
 * This source file is subject to the GNU General Public License (GPL-3.0).
@@ -7,10 +7,10 @@
 * https://opensource.org/licenses/GPL-3.0
 * 
 *
-* @copyright Copyright (c) 2021 Paige Jordan
+* @copyright Copyright (c) 2022 Paige Jordan
 * @license   GPL-3.0 GNU General Public License v3.0
 * @link      https://github.com/swishs-client-mod-plugins/cumcord-plugins/pronoun-bio-scraper
-*/const g=r.findByDisplayName("Flex"),l=r.findByProps("DropdownSizes"),E=r.findByProps("Sizes","Tags"),P=r.findByDisplayName("TextInput"),i=r.findByProps("ModalCloseButton"),m=r.findByProps("getUser","filter").getUser;var B=({e,author:n,extract:s})=>{const p=n in a.persist?a.persist[n]:s(m(n).bio),[d,c]=t.React.useState(p),h=o=>{o.key==="Enter"&&(a.persist[n]=d,e.onClose())};return t.React.useEffect(()=>(document.addEventListener("keydown",h),()=>document.removeEventListener("keydown",h))),t.React.createElement(i.ModalRoot,{transitionState:e.transitionState,className:"pronoun-overide-modal",size:"small"},t.React.createElement(M.ErrorBoundary,null,t.React.createElement(i.ModalHeader,{separator:!1},t.React.createElement(g.Child,{basis:"auto",grow:1,shrink:1,wrap:!1},t.React.createElement(E,{tag:"h2",size:E.Sizes.SIZE_20},"Override Pronouns")),t.React.createElement(g.Child,{basis:"auto",grow:0,shrink:1,wrap:!1},t.React.createElement(i.ModalCloseButton,{onClick:e.onClose}))),t.React.createElement(i.ModalContent,null,t.React.createElement(P,{onChange:o=>{console.log(o),c(o)},value:d})),t.React.createElement(i.ModalFooter,null,t.React.createElement(l,{onClick:()=>{a.persist[n]=d,e.onClose()},color:l.Colors.BRAND_NEW},"Change Pronouns"),t.React.createElement(l,{onClick:e.onClose,look:l.Looks.LINK,color:l.Colors.TRANSPARENT},"Cancel"))))};/**
+*/const P=r.findByDisplayName("SwitchItem");var m=()=>(y.useNest(s.persist),t.React.createElement(P,{note:"This may get you rate limited if you hop to a bunch of servers in quick succession but you should for the most part be fine. (Use at your own discretion)",onChange:()=>s.persist.store.fetch=!s.persist.ghost.fetch,value:s.persist.ghost.fetch},'Automatically Fetch Pronouns (get rid of "still loading")'));/**
 * NOTICE OF LICENSE
 *
 * This source file is subject to the GNU General Public License (GPL-3.0).
@@ -19,7 +19,19 @@
 * https://opensource.org/licenses/GPL-3.0
 * 
 *
-* @copyright Copyright (c) 2021 Paige Jordan
+* @copyright Copyright (c) 2022 Paige Jordan
 * @license   GPL-3.0 GNU General Public License v3.0
 * @link      https://github.com/swishs-client-mod-plugins/cumcord-plugins/pronoun-bio-scraper
-*/const{getMessage:x}=r.findByProps("getMessages"),{getUserProfile:v}=r.findByProps("getUserProfile"),{getChannelId:z}=r.findByProps("getVoiceChannelId"),{openModal:N}=r.findByProps("openModal","openModalLazy"),{getUser:U}=r.findByProps("getUser","getCurrentUser"),y=r.findByProps("MenuGroup","MenuItem"),I=r.find(e=>e.default?.displayName==="MessageTimestamp"),S=r.find(e=>e.default?.displayName==="MessageContextMenu"),C=e=>{if(!e)return;e=e.replace(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,"");let n=e.match(/[a-z]+?(?:\/[a-z]+)+/i);if(!n&&~e.indexOf("pronouns")){if(~e.indexOf("any"))return"any pronouns";if(~e.indexOf("avoid"))return"avoid pronouns, use my name";if(~e.indexOf("ask"))return"ask for pronouns"}if(n)return n[0]};let u=[];var T=()=>({onLoad(){u.push(f.after("default",I,([e],n)=>{let s=x(z(),e.id?.substring(18)),p=U(s?.author?.id),d=v(s?.author?.id),c=s?.author?.id in a.persist?a.persist[s.author.id]:d?C(p?.bio):"still loading";!c||u.push(f.after("children",n.props.children.props,(h,o)=>{if(o.props.children.props.children[1]!=="edited")return o.props.children.props.children[1]+=` \u2022 ${c}`,o}))})),u.push(f.after("default",S,([e],n)=>(R.findInReactTree(n,s=>s?.props?.id=="pronouns")||n.props.children.push(t.React.createElement(y.MenuGroup,null,t.React.createElement(y.MenuItem,{id:"pronouns",label:"Overide Pronouns",action:()=>N(s=>t.React.createElement(B,{e:s,author:e.message.author.id,extract:C}))}))),n)))},onUnload(){u.forEach(e=>e())}});return T})(cumcord.modules.webpack,cumcord.ui.components,cumcord.pluginData,cumcord.modules.common,cumcord.patcher,cumcord.utils);
+*/const g=r.findByDisplayName("Flex"),l=r.findByProps("DropdownSizes"),E=r.findByProps("Sizes","Tags"),B=r.findByDisplayName("TextInput"),d=r.findByProps("ModalCloseButton"),v=r.findByProps("getUser","filter").getUser;var x=({e,author:n,extract:o})=>{const p=n in s.persist?s.persist[n]:o(v(n).bio),[i,c]=t.React.useState(p),h=a=>{a.key==="Enter"&&(s.persist[n]=i,e.onClose())};return t.React.useEffect(()=>(document.addEventListener("keydown",h),()=>document.removeEventListener("keydown",h))),t.React.createElement(d.ModalRoot,{transitionState:e.transitionState,className:"pronoun-overide-modal",size:"small"},t.React.createElement(R.ErrorBoundary,null,t.React.createElement(d.ModalHeader,{separator:!1},t.React.createElement(g.Child,{basis:"auto",grow:1,shrink:1,wrap:!1},t.React.createElement(E,{tag:"h2",size:E.Sizes.SIZE_20},"Override Pronouns")),t.React.createElement(g.Child,{basis:"auto",grow:0,shrink:1,wrap:!1},t.React.createElement(d.ModalCloseButton,{onClick:e.onClose}))),t.React.createElement(d.ModalContent,null,t.React.createElement(B,{onChange:a=>{c(a)},value:i})),t.React.createElement(d.ModalFooter,null,t.React.createElement(l,{onClick:()=>{s.persist[n]=i,e.onClose()},color:l.Colors.BRAND_NEW},"Change Pronouns"),t.React.createElement(l,{onClick:e.onClose,look:l.Looks.LINK,color:l.Colors.TRANSPARENT},"Cancel"))))};/**
+* NOTICE OF LICENSE
+*
+* This source file is subject to the GNU General Public License (GPL-3.0).
+* You can find more information in the LICENSE.md file.
+* More information is also avaliable at this URL:
+* https://opensource.org/licenses/GPL-3.0
+* 
+*
+* @copyright Copyright (c) 2022 Paige Jordan
+* @license   GPL-3.0 GNU General Public License v3.0
+* @link      https://github.com/swishs-client-mod-plugins/cumcord-plugins/pronoun-bio-scraper
+*/const{getMessage:N}=r.findByProps("getMessages"),{fetchProfile:z}=r.findByProps("fetchProfile"),{getUserProfile:I}=r.findByProps("getUserProfile"),{getChannelId:S}=r.findByProps("getVoiceChannelId"),{openModal:U}=r.findByProps("openModal","openModalLazy"),{getUser:T}=r.findByProps("getUser","getCurrentUser"),C=r.findByProps("MenuGroup","MenuItem"),O=r.find(e=>e.default?.displayName==="MessageTimestamp"),A=r.find(e=>e.default?.displayName==="MessageContextMenu"),M=e=>{if(!e)return;e=e.replace(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,"");let n=e.match(/[a-z]+?(?:\/[a-z]+)+/i);if(!n&&~e.indexOf("pronouns")){if(~e.indexOf("any"))return"any pronouns";if(~e.indexOf("avoid"))return"avoid pronouns, use my name";if(~e.indexOf("ask"))return"ask for pronouns"}if(n)return n[0]};let u=[];var L=()=>({onLoad(){u.push(f.after("default",O,([e],n)=>{let o=N(S(),e.id?.substring(18)),p=T(o?.author?.id),i=I(o?.author?.id),c=o?.author?.id in s.persist?s.persist[o.author.id]:i?M(p?.bio):"still loading";!c||(!i&&s.persist.ghost.fetch&&z(o?.author?.id),u.push(f.after("children",n.props.children.props,(h,a)=>{if(a.props.children.props.children[1]!=="edited")return a.props.children.props.children[1]+=` \u2022 ${c}`,a})))})),u.push(f.after("default",A,([e],n)=>(y.findInReactTree(n,o=>o?.props?.id=="pronouns")||n.props.children.push(t.React.createElement(C.MenuGroup,null,t.React.createElement(C.MenuItem,{id:"pronouns",label:"Overide Pronouns",action:()=>U(o=>t.React.createElement(x,{e:o,author:e.message.author.id,extract:M}))}))),n)))},onUnload(){u.forEach(e=>e())},settings:t.React.createElement(m,null)});return L})(cumcord.modules.webpack,cumcord.pluginData,cumcord.utils,cumcord.modules.common,cumcord.ui.components,cumcord.patcher);
