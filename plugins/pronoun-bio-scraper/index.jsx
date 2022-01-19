5c1dd10a5d69c93540b64200703e6649dcac90f6
Swishilicous <52143580+Swishilicous@users.noreply.github.com>
2022-01-18 19:36:44 -0600

    fix 2???

--- plugins/pronoun-bio-scraper/index.jsx
+++ plugins/pronoun-bio-scraper/index.jsx
@@ -5,7 +5,7 @@
  * You can find more information in the LICENSE.md file.
  * More information is also avaliable at this URL:
  * https://opensource.org/licenses/GPL-3.0
- * 
+ *
  *
  * @copyright Copyright (c) 2022 Paige Jordan
  * @license   GPL-3.0 GNU General Public License v3.0
@@ -31,7 +31,7 @@ const { getUser: getCachedUser } = findByProps('getUser', 'getCurrentUser');
 const Menu = findByProps('MenuGroup', 'MenuItem');
 const MessageTimestamp = find(m => m.default?.displayName === 'MessageTimestamp');
 
-const extractPronouns = (bio) => {
+const extractPronouns = bio => {
   if (!bio) return;
   bio = bio.replace(/(http(s)?:\/\/.)?(www\.)?[-a-z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-z0-9@:%_\+.~#?&//=]*)/gi, '');
   let match = bio.match(/[a-z]+?(?:\/[a-z]+)+/i);
@@ -39,25 +39,27 @@ const extractPronouns = (bio) => {
     if (~bio.indexOf('any')) return 'any pronouns';
     if (~bio.indexOf('avoid')) return 'avoid pronouns, use my name';
     if (~bio.indexOf('ask')) return 'ask for pronouns';
-  };
-  if (match) return match[0];
+    if (~bio.indexOf('male')) return 'probably he/him';
+    if (~bio.indexOf('female')) return 'probably she/her';
+  }
+  if (match) return match[0].toLowerCase();
 };
 
 // credits to juby and i think xinos
 let unpatch = [];
 const lazyPatchContextMenu = async (displayName, patch) => {
-  const filter = (m) => m.default && m.default.displayName === displayName;
+  const filter = m => m.default && m.default.displayName === displayName;
   const m = find(filter, false);
   if (m) patch(m);
   else {
     const module = findByProps('openContextMenuLazy');
     unpatch.unshift(
-      before('openContextMenuLazy', module, (args) => {
+      before('openContextMenuLazy', module, args => {
         const lazyRender = args[1];
         args[1] = async () => {
           const render = await lazyRender(args[0]);
 
-          return (config) => {
+          return config => {
             const menu = render(config);
             if (menu?.type?.displayName === displayName && patch) {
               unpatch[0]();
@@ -84,26 +86,26 @@ export default () => {
         let author = getCachedUser(message.author.id);
         let unloaded = !getUserProfile(message.author.id);
 
-        if (persist.ghost.fetch && !loaded.includes(message.id))
-          loaded.push(message.id);
+        if (persist.ghost.fetch && !loaded.includes(message.id)) loaded.push(message.id);
 
-        let bio = message.author.id in persist
-          ? persist[message.author.id]
-          : !unloaded
-            ? extractPronouns(author.bio)
+        let bio = message.author.id in persist 
+          ? persist[message.author.id] 
+          : !unloaded 
+            ? extractPronouns(author.bio) 
             : 'still loading';
         if (!bio) return;
 
-        if (unloaded && persist.ghost.fetch
-          && !(fetched.includes(message.author.id))
-          && loaded.includes(message.id)
+        if (unloaded && persist.ghost.fetch 
+          && !fetched.includes(message.author.id) 
+          && loaded.includes(message.id) 
           && !FluxDispatcher._currentDispatchActionType) {
           fetched.push(message.author.id);
           fetchProfile(message.author.id);
         }
 
+        if (!res?.props?.children?.props) return;
         unpatch.push(after('children', res.props.children.props, (_, res) => {
-          if (res.props.children?.props.children?.[1] === 'edited') return;
+          if (res?.props?.children?.props?.children?.[1] === 'edited') return;
           res.props.children.props.children[1] += ` • ${bio}`;
           return res;
         }));
@@ -115,8 +117,9 @@ export default () => {
             res.props.children.push(
               <Menu.MenuGroup>
                 <Menu.MenuItem
-                  id='pronouns' label='Overide Pronouns'
-                  action={() => openModal((e) =>
+                  id='pronouns'
+                  label='Overide Pronouns'
+                  action={() => openModal(e => 
                     <OverrideModal e={e} author={args.message.author.id} extract={extractPronouns} />
                   )}
                 />
@@ -127,6 +130,6 @@ export default () => {
       });
     },
     onUnload() { unpatch.forEach(unpatch => unpatch()); },
-    settings: <Settings />
+    settings: <Settings />,
   };
 };
