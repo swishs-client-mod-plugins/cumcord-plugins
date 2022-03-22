97a0be685562df5947d07d5cdfb5a574297004a6
Swishilicous <52143580+Swishilicous@users.noreply.github.com>
2022-03-21 21:38:28 -0500

    iryis plugin

---
+++ plugins/ignore-stars/index.tsx
@@ -0,0 +1,35 @@
+/* The below code is licensed under MIT. */
+
+import type { MessageJSON } from 'discord-types';
+
+import { findByProps } from '@cumcord/modules/webpack';
+import { FluxDispatcher } from '@cumcord/modules/common';
+
+const UnreadStore = findByProps('getUnreadCount');
+const ChannelStateStore = findByProps('isChannelMuted');
+const UnreadActions = findByProps('ackCategory', 'ack');
+
+// cba to add thread support
+const onMessage = (event: {
+  channelId: string;
+  isPushNotification: boolean;
+  message: MessageJSON;
+  optimistic: boolean;
+}) => {
+  if (
+    !event.message ||
+    event.optimistic ||
+    UnreadStore.getUnreadCount(event.channelId) > 2 ||
+    !event.message.embeds?.[0]?.title?.includes('star') ||
+    ChannelStateStore.isChannelMuted(event.message.guild_id, event.channelId)
+  ) return;
+
+  UnreadActions.ack(event.channelId, true, true);
+};
+
+export default () => {
+  return {
+    onLoad: () => FluxDispatcher.subscribe('MESSAGE_CREATE', onMessage),
+    onUnload: () => FluxDispatcher.unsubscribe('MESSAGE_CREATE', onMessage)
+  };
+};
